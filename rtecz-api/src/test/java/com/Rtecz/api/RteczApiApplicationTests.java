710ad5deffec29d24e2514b15fedb90a180eb4ea
Nishi Nanda Singh <n.nandasingh221@gmail.com>
2025-07-03 22:47:52 +0530

    committed RTecz

---
+++ rtecz-api/src/test/java/com/Rtecz/api/RteczApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rtecz.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RteczApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
